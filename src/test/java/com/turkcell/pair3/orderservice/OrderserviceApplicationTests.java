8fb4c8834117d242c1ccc542a38330c1510461a3
emreOytun <emreoytun58@gmail.com>
2024-08-29 22:22:31 +0300

    mockito extend added to test class
    (cherry picked from commit 5b4f425b1489528658ba32a34d279a2f6dfb5a42)

--- src/test/java/com/turkcell/pair3/orderservice/OrderserviceApplicationTests.java
+++ src/test/java/com/turkcell/pair3/orderservice/OrderserviceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.turkcell.pair3.orderservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = OrderserviceApplication.class)
+@ExtendWith(MockitoExtension.class)
 class OrderserviceApplicationTests {
 
     @Test
